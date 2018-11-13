b19e3be6581dfcf84ed15bf7cadbb0495ff40a17
David Voihanski <voihanskidavid@gmail.com>
2018-11-13 18:56:30 +0200

    organized some stuff

--- Matala1/src/myMath/LinePlotTest.java
+++ Matala1/src/myMath/LinePlotTest.java
@@ -16,11 +16,10 @@ public class LinePlotTest extends JFrame {
 	public LinePlotTest() {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(800, 1000);
-		// Insert rest of the code here
 	}
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 		Polynom p=new Polynom("0.2x^4-1.5x^3+3.0x^2-x-5");
 		LinePlotTest frame = new LinePlotTest();
 		DataTable data = new DataTable(Double.class, Double.class);
@@ -28,28 +27,7 @@ public class LinePlotTest extends JFrame {
 			double y = p.f(x);
 			data.add(x, y);
 		}
-=======
-		 LinePlotTest frame = new LinePlotTest();
-		 Polynom p=new Polynom("0.2x^4-1.5x^3+3.0x^2-1x^1-5x^0");
-		 DataTable data = new DataTable(Double.class, Double.class);
-		 for (double x = -2.0; x <= 6.0; x+=0.10) {
-		 double y = p.f(x);
-		 data.add(x, y);
-		 }
 		
-		 XYPlot plot = new XYPlot(data);
-		 frame.getContentPane().add(new InteractivePanel(plot));
-		 LineRenderer lines = new DefaultLineRenderer2D();
-		 plot.setLineRenderers(data, lines);
-		 Color color = new Color(0.0f, 0.3f, 1.0f);
-		 DataTable min=new DataTable(Double.class,Double.class);
-		 color=color.red;
-		 min.add(0.0,0.0);
-		 plot.add(min);
-		 plot.getPointRenderers(min).get(0).setColor(color);
-		 frame.setVisible(true);
->>>>>>> 22fa05fd66aa30b81832f80a61e75ff9119b0f96
-
 		XYPlot plot = new XYPlot(data);
 		frame.getContentPane().add(new InteractivePanel(plot));
 		LineRenderer lines = new DefaultLineRenderer2D();
@@ -71,14 +49,7 @@ public class LinePlotTest extends JFrame {
 	    plot.add(globalExtremum);
 	    plot.getPointRenderers(extremum).get(0).setColor(minMax);
 	    plot.getPointRenderers(globalExtremum).get(0).setColor(GlobalMinMax);
-	    
-		//		 DataTable min=new DataTable(Double.class,Double.class);
-		//		 color=color.red;
-		//		 for (double x = -5.0; x <= 5.0; x+=0.10) {
-		//		 min.add(0.0,0.0);
-		//		 }
-		//		 plot.add(min);
-		//		 plot.getPointRenderers(min).get(0).setColor(color);
+
 		    	 frame.setVisible(true);
 	}
 }
