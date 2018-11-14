114faf6f9a8b0c12f9a790260cfb722d2fc4023c
David Voihanski <voihanskidavid@gmail.com>
2018-11-14 15:18:58 +0200

    Had 2 main classes, fixed

--- Matala1/src/myMath/DrawPoly.java
+++ Matala1/src/myMath/DrawPoly.java
@@ -1,4 +1,3 @@
-
 package myMath;
 
 import java.awt.Color;
@@ -14,44 +13,56 @@ import de.erichseifert.gral.plots.lines.LineRenderer;
 import de.erichseifert.gral.ui.InteractivePanel;
 
 public class DrawPoly extends JFrame {
+	/**
+	 * default constructor 
+	 */
 	public DrawPoly() {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setSize(800, 1000);
+		setSize(800, 800);
 	}
-
-	public static void main(String[] args) {
-
-		Polynom p = new Polynom("x^2");
-		PlotTest frame = new PlotTest();
-		DataTable data = new DataTable(Double.class, Double.class);
-		for (double x = -2.0; x <= 6.0; x += 0.25) {
-			double y = p.f(x);
-			data.add(x, y);
+	/**
+	 * Draws a given polynom's graph local extremum are drawn in red, global are drawn in green.
+	 * @param x0 the starting point to draw from
+	 * @param x1 the end point
+	 * @param p	 Polynom type param you wish to draw
+	 */
+	public static void draw(double x0, double x1, Polynom p) {
+		DrawPoly frame = new DrawPoly();
+		DataTable data = new DataTable(Double.class, Double.class);  //data table to hold our function's points
+		while(x0+0.10<=x1) {			 //finds points on the function with a difference of 0.10 in between
+			double y = p.f(x0);  
+			data.add(x0, y);
+			x0=x0+0.10;
 		}
-
+		if(x0<x1)data.add(x1,p.f(x1));   //if we didn't get to the x1 border adds this point
 		XYPlot plot = new XYPlot(data);
 		frame.getContentPane().add(new InteractivePanel(plot));
 		LineRenderer lines = new DefaultLineRenderer2D();
 		plot.setLineRenderers(data, lines);
-		Color minMax = new Color(0.0f, 0.3f, 1.0f);
-		minMax = Color.RED;
-		Color GlobalMinMax = new Color(0.0f, 0.3f, 1.0f);
-		GlobalMinMax = Color.pink;
-		DataTable extremum = new DataTable(Double.class, Double.class);
-		DataTable globalExtremum = new DataTable(Double.class, Double.class);
-		ArrayList<Point> temp = p.findMinMax(-2, 6);
-		Iterator<Point> it = temp.iterator();
-		while (it.hasNext()) {
-			Point currP = it.next();
-			if (currP.isGlobalMax() || currP.isGlobalMin())
-				globalExtremum.add(currP.getX(), currP.getY());
-			else
-				extremum.add(currP.getX(), currP.getY());
-		}
-		plot.add(extremum);
-		plot.add(globalExtremum);
-		plot.getPointRenderers(extremum).get(0).setColor(minMax);
-		plot.getPointRenderers(globalExtremum).get(0).setColor(GlobalMinMax);
-		frame.setVisible(true);
+	    Color minMax = new Color(0.0f, 0.3f, 1.0f);
+	    minMax=Color.RED;
+	    Color GlobalMinMax = new Color(0.0f, 0.3f, 1.0f);
+	    GlobalMinMax=Color.GREEN;
+	    DataTable extremum = new DataTable(Double.class, Double.class);     //datatable to hold our extremum points
+	    DataTable globalExtremum = new DataTable(Double.class, Double.class); //data table to hold our global extremum
+	    ArrayList<Point>temp=p.findMinMax(-2, 6);							//finds the extremums
+	    Iterator<Point>it=temp.iterator();
+	    while(it.hasNext()) {												//adds them to their corresponding tables
+	    	Point currP=it.next();
+	    	if(currP.isGlobalMax()||currP.isGlobalMin()) globalExtremum.add(currP.getX(),currP.getY());
+	    	else extremum.add(currP.getX(),currP.getY());
+	    }
+	    plot.add(extremum);												//adds the extremum to our graph
+	    plot.add(globalExtremum);										//adds the global extremum to our graph
+	    plot.getPointRenderers(extremum).get(0).setColor(minMax);		//colors extremum in red
+	    plot.getPointRenderers(globalExtremum).get(0).setColor(GlobalMinMax);	//colors the global in green
+        frame.setVisible(true);													//shows the graph
+	}
+
+	public static void main(String[] args) {
+
+		Polynom p=new Polynom("0.2x^4-1.5x^3+3.0x^2-x-5");
+		draw(-2,6,p);
+
 	}
 }
